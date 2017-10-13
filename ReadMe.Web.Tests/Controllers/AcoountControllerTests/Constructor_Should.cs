dbcbabe6caa21279ac70303263042c9b0c012de2
Simona Tsenova <simona.tsenova17@gmail.com>
2017-10-13 09:51:52 +0300

    Removed coveralls

---
+++ ReadMe.Web.Tests/Controllers/AcoountControllerTests/Constructor_Should.cs
@@ -0,0 +1,12 @@
+﻿using Moq;
+using NUnit.Framework;
+using ReadMe.Services.Contracts;
+using ReadMe.Web.Controllers;
+using System;
+
+namespace ReadMe.Web.Tests.Controllers.AcoountControllerTests
+{
+    class Constructor_Should
+    {
+    }
+}
